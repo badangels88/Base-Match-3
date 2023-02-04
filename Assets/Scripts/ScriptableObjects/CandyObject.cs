e1a93528f6e812222966eb012cea2e2623c9f823
Salvatore Vigliotta <salvatore.vigliotta@outlook.it>
2023-02-04 18:32:43 +0100

    added base gameplay

---
+++ Assets/Scripts/ScriptableObjects/CandyObject.cs
@@ -0,0 +1,9 @@
+using UnityEngine;
+
+[CreateAssetMenu(fileName = "CandyObject", menuName = "Custom/Object")]
+public class CandyObject : ScriptableObject
+{
+    public GameObject BasePrefab;
+    public GameObject ExposionPrefab;
+    public GameObject BonusPrefab;
+}
---
+++ Assets/Scripts/SoundManager.cs
@@ -0,0 +1,27 @@
+﻿using UnityEngine;
+
+public class SoundManager : MonoBehaviour
+{
+    //crincle sound found here: http://freesound.org/people/volivieri/sounds/37171/
+    public AudioClip crincleAudioClip;
+    private AudioSource crincle;
+
+    void Awake()
+    {
+        crincle = AddAudio(crincleAudioClip);
+    }
+
+    AudioSource AddAudio(AudioClip audioClip)
+    {
+        AudioSource audioSource = this.gameObject.AddComponent<AudioSource>();
+        audioSource.playOnAwake = false;
+        audioSource.clip = audioClip;
+        return audioSource;
+    }
+
+    public void PlayCrincle()
+    {
+        if (crincle != null)
+            crincle.Play();
+    }
+}
